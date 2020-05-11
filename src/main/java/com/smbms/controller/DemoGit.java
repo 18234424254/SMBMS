8cace3ec0a90495d265c251d21cc5b5a9901054b
小宋 <1132443815@qq.com>
2020-05-11 15:11:31 +0800

    dev11111

--- src/main/java/com/smbms/controller/DemoGit.java
+++ src/main/java/com/smbms/controller/DemoGit.java
@@ -4,4 +4,7 @@ public class DemoGit {
     public void test1(){
         System.out.println("1111111111111111");
     }
+    public void test2(){
+        System.out.println("2222222222222222");
+    }
 }
